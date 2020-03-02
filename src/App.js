0d9b66078dcea48e9d868276b0e357ce38be5b15
juniordugue <jrdugue@gmail.com>
2020-03-01 21:38:39 -0500

    added styling, removed favicon, logo files, created component folders with five functional components to start with

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from './components/incomeExpenses';
+import { TransactionList } from "./components/transactionList";
+import { AddTransaction } from "./components/addTransaction";
 
 function App() {
   return (
     <div>
- 
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
